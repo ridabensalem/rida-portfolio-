f4429eb1c75add19b258070b00bfce8701723ae8
rida bensalem <ridasbnesalmas@gmail.com>
2023-04-06 15:15:47 +0000

    created navbar section

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+function Navbar() {
+    return(
+            <nav className="main-bg">
+               <h1>Hi Rida</h1>
+            </nav>
+    )
+}
+export default Navbar;
