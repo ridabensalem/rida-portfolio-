f21fbaef701965ca9a5d0913bde246489a40632d
rida bensalem <ridasbnesalmas@gmail.com>
2023-04-10 14:11:38 +0000

    Create navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
 function Navbar() {
     return(
-            <nav className="main-bg">
-               <h1>Hi Rida</h1>
+            <nav className=" bg-main-bg-dark md:sticky top-0 z-10  p-5 flex flex-row place-content-between" >
+               <div className="logo  font-main-font text-coding-color text-2xl font-bold">RIDA</div>
+               <div className="navigation text-white  ">
+                <ul className='flex flex-row place-content-around font-coding-font gap-x-4 text-gray-light text-xs p-2 '>
+                    <li><a href="#"> <span className='text-coding-color '>1.</span>About</a></li>
+                    <li><a href="#"> <span  className='text-coding-color'>2.</span>Experience</a></li>
+                    <li><a href=""><span className='text-coding-color'>3.</span>Skills</a></li>
+                    <li><a href=""> <span className='text-coding-color'>4.</span>Projects </a></li>
+                    <li><a href=""><span className='text-coding-color'>5.</span>Contact</a></li>
+                </ul>
+               </div>
             </nav>
     )
 }
