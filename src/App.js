e1d8b930f1d9c498c02254bfad467327e5cded0d
rida bensalem <ridasbnesalmas@gmail.com>
2023-04-11 13:21:16 +0000

    Called About section in the app file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Navbar from './components/Navbar';
+import About from './components/About';
 function App() {
   return (
     <main>
       <Navbar />
+      <About />
     </main>
   );
 }
