2106d55680cfd15cd860bd53e07c595a7b791000
rida bensalem <ridasbnesalmas@gmail.com>
2023-04-13 12:05:45 +0000

    added About file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import Navbar from './components/Navbar';
-import About from './components/About';
+import Intro from './components/Intro';
 function App() {
   return (
     <main>
       <Navbar />
-      <About />
+      <Intro />
     </main>
   );
 }
