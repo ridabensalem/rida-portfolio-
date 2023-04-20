a211f69229d0a5000752501a2216f986db6ec097
rida bensalem <ridasbnesalmas@gmail.com>
2023-04-20 12:35:26 +0000

    Import experience components

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import Intro from './components/Intro';
 import About from './components/About';
+import Experience from './components/Experience';
 function App() {
   return (
     <main>
       <Navbar />
       <Intro />
       <About />
+      <Experience />
     </main>
   );
 }
