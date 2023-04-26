96b0d38732edd2fa217c4f6a0748b6e2bcc90e96
rida bensalem <ridasbnesalmas@gmail.com>
2023-04-26 10:03:43 +0100

    Created Experience section

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,23 +1,57 @@
 import React from "react";
-function Experience() {
-return(
-    <div className="experience-container content-center flex flex-col p-36" id="about">
-            <h1 className="text-header-color font-main-font text-3xl font-bold">
-                <span className="font-coding-font text-coding-color font-normal ">  2.</span> Experience</h1>
-            <hr  className="w-1/4  border-t-1 border-separator my-4 opacity-50 border-thin"/>  
-            <div className="content-container  flex flex-row  justify-between">
-            
-            <div className="content pt-10 text-white w-1/2 flex">
-                <p>
-                Hello, my name is Rida and I am a  <span className="text-coding-color"> full-stack developer </span> with expertise in JavaScript, React, Redux, and Rails.
-                I am a self employed developer and tech enthusiast who is passionate about building scalable and efficient web applications.
-                I am currently working on a few personal projects, I am looking for a full time jon as a <span className="text-coding-color">full stack developer</span>
-                I love to contribute to  <span className="text-coding-color"> open source projects</span> .
-                </p>
-               
+import { FaBriefcase } from "react-icons/fa";
+
+const Experience = () => {
+    return (
+        <section id="experience" className="py-20">
+            <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
+                <div className=" md:flex-row md:items-center">
+                    <div className="experience-container  flex flex-row md:w-1/5 text-center  px-1 md:text-left ">
+                        <FaBriefcase className=" w-10 h-10  text-coding-color mb-2" />
+                        <h2 className="text-3xl font-extrabold text-white px-2 pt-1 ">Experience</h2>
+                    </div>
+                    <div className="md:w-4/5">
+                        <div className="space-y-4 experience-1">
+                            <div className="bg-white shadow-lg rounded-lg overflow-hidden">
+                                <div className="px-4 py-5 sm:p-6">
+                                    <h3 className="text-lg font-medium text-gray-900">Self employed Full Stack Developer</h3>
+                                    <p className="mt-1 text-gray-600 text-sm">June 2020 - Present</p>
+                                    <ul className="list-disc list-inside mt-3 text-gray-600 text-sm">
+                                        <li>Developed custom web solutions using a variety of programming languages and frameworks</li>
+                                        <li>Created static and dynamic websites for clients that received positive feedback and improved their online presence.</li>
+                                        <li>Fixed bugs and optimized website performance, resulting in faster loading times and better user experience.</li>
+                                        <li>Integrated various API services, such as GitHub and others API, to display project details.</li>
+                                        <li>Deployed and maintained websites on Netlify, ensuring high availability and fast loading times.</li>
+
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="md:w-4/5">
+                        <div className="space-y-4 pt-4 experience-2">
+                            <div className="bg-white shadow-lg rounded-lg overflow-hidden">
+                                <div className="px-4 py-5 sm:p-6">
+                                    <h3 className="text-lg font-medium text-gray-900">Web Developer Intern at SmartSoluce</h3>
+                                    <p className="mt-1 text-gray-600 text-sm">May 2022</p>
+                                    <ul className="list-disc list-inside mt-3 text-gray-600 text-sm">
+                                            <li>Designed and developed custom WordPress themes to match client specifications</li>
+                                            <li>Built and maintained sites in WordPress CMS</li>
+                                            <li>Collaborated with other developers and designers on project teams</li>
+                                            <li>Communicated with clients and stakeholders to understand their needs and provide updates on the status of projects</li>
+                                            <li>Provided technical support to clients as needed</li>
+                                            <li>Ranked a customer website among the first pages on Google based on Agadir city</li>
+                                            <li>Developed WordPress websites that kept clients always satisfied</li>
+                                            <li>Created websites with great designs, making them competitive with other websites in the same field</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-           </div>    
-        </div>
-)
-}
-export  default Experience;
+        </section>
+    );
+};
+
+export default Experience;
