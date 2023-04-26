b1d1a8b322e63342fab4cae58c27575a6b95ae76
Rida Bensalem <ridasbnesalmas@gmail.com>
2023-04-26 16:33:22 +0100

    Import contact component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import About from './components/About';
 import Experience from './components/Experience';
 import Projects from './components/Projects';
 import Skills from './components/Skills';
+import Contact from './components/Contact';
 function App() {
   return (
     <main>
@@ -14,6 +15,7 @@ function App() {
       <Experience />
       <Projects />
       <Skills />
+      <Contact />
     </main>
   );
 }
