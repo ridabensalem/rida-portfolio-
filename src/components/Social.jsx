0b381523e386a6da828215794e9fbc2d3413c2ca
Rida Bensalem <ridasbnesalmas@gmail.com>
2023-04-27 16:07:55 +0100

    Create Sociale Contact component

---
+++ src/components/Social.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BsGithub, BsLinkedin, BsTwitter, } from 'react-icons/bs';
+function Social() {
+    return (
+        <div className="social-container text-header-color text-3xl flex flex-row justify-between pt-16">
+            <a href="https://github.com/ridabensalem" target='_blank'> <BsGithub className="social-icon" /></a>
+            <a href="https://www.linkedin.com/in/rida-bensalem/" target='_blank'>
+                <BsLinkedin className="social-icon" />
+            </a>
+            <a href="https://twitter.com/coder_rida" target='_blank'>
+                <BsTwitter className="social-icon" />
+            </a>
+        </div>
+    )
+}
+export default Social;
