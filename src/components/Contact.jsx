9afcd634c475e3b2f933769b895dcd0ac146dcb2
Rida Bensalem <ridasbnesalmas@gmail.com>
2023-04-27 16:33:32 +0100

    Format  file

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,7 +3,7 @@ import Social from "./Social";
 const Contact = () => {
 
     const handleSubmit = () => {
-        window.location.href="mailto:ridasbnesalmas@gmail.com"
+        window.location.href = "mailto:ridasbnesalmas@gmail.com"
     };
 
     return (
@@ -13,11 +13,11 @@ const Contact = () => {
             </div>
             <div className="contact text-white w-1/2 text-center mx-auto pt-10">
                 <p>I am currently open to new work opportunities and I welcome collaboration with other developers. If you are looking for a skilled full stack developer, please feel free to reach out to me.
-                     I am passionate about creating innovative solutions and would be honored to contribute to your project. </p>
-                     <div className="about-buttons mx-auto w-1/4 pt-9 text-gray-light">
-          <button  onClick={handleSubmit} className="bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 hover:text-white rounded text-lg">Say Hello  </button>
-           <Social/>       
-        </div>
+                    I am passionate about creating innovative solutions and would be honored to contribute to your project. </p>
+                <div className="about-buttons mx-auto w-1/4 pt-9 text-gray-light">
+                    <button onClick={handleSubmit} className="bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 hover:text-white rounded text-lg">Say Hello  </button>
+                    <Social />
+                </div>
             </div>
 
         </div>
