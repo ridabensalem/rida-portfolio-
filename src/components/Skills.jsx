b3bdea2a5daa677a49daf4fd24a3059ac061ca8c
Rida Bensalem <ridasbnesalmas@gmail.com>
2023-05-08 13:25:56 +0100

    Create mobile version for skills  section

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -7,7 +7,7 @@ function Skills() {
             <div className="container px-5 py-10 mx-auto">
                 <div className="text-center  text-white mb-20">
                     <BiChip className="text-coding-color text-4xl inline-block mb-4" />
-                    <h1 className="sm:text-4xl text-3xl font-medium title-font text-white mb-4">
+                    <h1 className=" skills-header sm:text-4xl text-3xl font-medium title-font text-white mb-4">
                         Skills &amp; Technologies
                     </h1>
                     <p className="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">
