514c89e685f2cf1d3840e9803163c8ec099f3773
Rida Bensalem <ridasbnesalmas@gmail.com>
2023-05-08 13:21:58 +0100

    Create mobile version of Intor section

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import { TypeAnimation } from 'react-type-animation';
 function Intro() {
   return (
-    <div className="about-container flex-col p-28">
-      <div className="about-header  text-2xl font-bold py-1">
+    <div className="intro-container flex-col p-28 ">
+      <div className="intro-header  text-2xl font-bold py-1">
         <h1 className="text-sm text-coding-color font-coding-font">Hi there, I am </h1>
-        <h2 className="text-7xl text-gray-light pt-5 font-main-font">Rida Bensalem</h2>
-        <h2 className="text-5xl text-header-color font-main-font pt-2 ">
+        <h2 className="text-7xl text-gray-light pt-5 font-main-font dev-name">Rida Bensalem</h2>
+        <h2 className="text-5xl text-header-color font-main-font pt-2 dev-animation">
           <TypeAnimation
             sequence={[
               'I Build sleek web applications',
@@ -22,12 +22,12 @@ function Intro() {
           />
         </h2>
       </div>
-      <div className="about__content  ">
+      <div className="intro__content  ">
         <p className="text-gray-light text-sm font-coding-font pt-9  ">
           I am a <span className="text-coding-color">full stack developer </span>who build sleek and responsive web applications <br /> currently I am working as
           a self employed developer and I am looking for a full time job as a <span className="text-coding-color">full stack developer </span>.
         </p>
-        <div className="about-buttons  flex gap-8 w-1/4 pt-9 text-gray-light">
+        <div className="intro-buttons  flex gap-8 w-1/4 pt-9 text-gray-light">
           <button className="bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 hover:text-white rounded text-lg">Hire me </button>
           <button className="bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 hover:text-white rounded text-lg">See Projects </button>
         </div>
