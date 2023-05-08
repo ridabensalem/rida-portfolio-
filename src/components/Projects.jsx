79f8ebf696edc9dedbff07119c5cbbe2a63fdc1d
Rida Bensalem <ridasbnesalmas@gmail.com>
2023-05-08 13:25:36 +0100

    Create mobile version for project section

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -11,8 +11,8 @@ return (
         <div className="icon-container flex justify-center">
         <HiOutlineCode className="text-coding-color  text-6xl  "/>
         </div>
-        <h2 className="text-3xl font-bold mb-10 text-white  text-center">My Projects</h2>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
+        <h2 className="text-3xl font-bold mb-10 text-white project-header text-center">My Projects</h2>
+        <div className=" project-card-container grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
           {projects.map(project => (
             <ProjectCard key={project.id} project={project} />
           ))}
