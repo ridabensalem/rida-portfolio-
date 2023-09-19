562eb18425eba28d6c4255328b6b39d76202c2da
Rida Bensalem <ridasbnesalmas@gmail.com>
2023-09-19 12:24:35 +0100

    Edit social icons

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BsGithub, BsLinkedin, BsTwitter, } from 'react-icons/bs';
 function Social() {
     return (
-        <div className="social-container text-header-color text-3xl flex flex-row justify-between pt-16">
+        <div className="social-container text-header-color text-3xl flex flex-row gap-24 pb-16 pt-16">
             <a href="https://github.com/ridabensalem" target='_blank'> <BsGithub className="social-icon" /></a>
             <a href="https://www.linkedin.com/in/rida-bensalem/" target='_blank'>
                 <BsLinkedin className="social-icon" />
